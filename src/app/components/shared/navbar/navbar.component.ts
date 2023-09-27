057b4b7e505730d072763b23ae6765a95a7437ed
H-410M-H <hassaanazam7@gmail.com>
2023-09-28 01:43:01 +0500

    Navbar Completed

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  mobileMenuOpen: boolean = false;
 
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
